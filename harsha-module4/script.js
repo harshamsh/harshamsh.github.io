0312013fea2176360be22aaff506aac5c9f3de63
harshamsh <msriharsha2@gmail.com>
2021-01-31 14:05:35 +0530

    final change

--- harsha-module4/script.js
+++ harsha-module4/script.js
@@ -1,9 +1,9 @@
 var names = ["yash" , "harsha" ,"jatin", "Yamen" , "Musunuru","Jackie","Jason" ]
 for(var i=0;i<names.length; i++){
 	if(names[i].charAt(0)=='j'||names[i].charAt(0)=='J'){
-		console.log("Goodbye " + names[i]);
+		byeSpeaker.sayBye(names[i])
 	}
 	else{
-		console.log("Hello " + names[i]);
+		helloSpeaker.sayHello(names[i]);
 	}
 }
